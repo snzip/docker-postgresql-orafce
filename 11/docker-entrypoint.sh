3a706ed18315dd42e227b11091708111978f8241
michael <zi-peng.song@microfocus.com>
2021-01-07 18:55:55 +0800

    - add redis

--- 11/docker-entrypoint.sh
+++ 11/docker-entrypoint.sh
@@ -4,8 +4,6 @@ set -Eeo pipefail
 declare -g DATABASE_ALREADY_EXISTS
 # look specifically for PG_VERSION, as it is expected in the DB dir
 
-# echo $PGDATA
-# ls -l $PGDATA
 export POSTGRES_PASSWORD=postgres
 
 if [ -s "$PGDATA/PG_VERSION" ]; then
@@ -14,20 +12,18 @@ fi
 
 if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
 	su - postgres -c "/var/lib/postgresql/bin/initdb -D $PGDATA --username=postgres --pwfile=<(echo "$POSTGRES_PASSWORD")"
-	
-	 cp /pg_conf/postgresql.conf  $PGDATA/
-	 cp /pg_conf/pg_hba.conf  $PGDATA/
+
+	cp /pg_conf/postgresql.conf $PGDATA/
+	cp /pg_conf/pg_hba.conf $PGDATA/
 
 	echo
 	echo 'PostgreSQL init process complete; ready for start up.'
 	echo
 else
-	ls $PGDATA
 	echo
 	echo 'PostgreSQL Database directory appears to contain a database; Skipping initialization'
 	echo
 fi
 
 su - postgres -c "/var/lib/postgresql/bin/pg_ctl -D  $PGDATA  -l  $PGDATA/logfile.txt start"
-tail -f  $PGDATA/logfile.txt
-
+tail -f $PGDATA/logfile.txt
